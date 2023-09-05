80b77b62f50847c0b95860c6b440ba8ce6320f2e
Saeed <Saeed@MacBook-Air.local>
2023-09-05 14:58:05 +0330

    fix login and login required

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 from flask import Flask, flash ,Response,  render_template, request , redirect
-from flask_login import LoginManager, login_user, UserMixin
-
+from flask_login import UserMixin ,current_user, LoginManager,login_user, logout_user,login_required
 login_manager = LoginManager()
 app = Flask(__name__)
-#login_manager.init_app(app)
+login_manager.init_app(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+login_manager.login_view = "login"
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
 
 class User(UserMixin):
 
     def __init__(self, id):
         self.id = id
-    def is_active (self):
-        return(True)
+
     def __repr__(self):
-        return "%d" % (self.id)
+        return (self.id)
 
 
-user = User(1)
+user = User(0)
+
+
+
 
 @app.route('/')
 @app.route('/index.html')
@@ -35,20 +33,51 @@ def about():
 def pages (page):
     page_url = page + '.html'
     return render_template(page_url)
+
+
+@app.route('/page3.html')
+@login_required
+def blog ():
+    titel = "بلاگ اول"
+    blog_body = "خب اینم از شروع بلاگ"
+
+    return render_template('page3.html' , title = titel, body = blog_body)
+
+
+
+
 @app.route('/login.html', methods=['GET', 'POST'] )
 def login ():
     if request.method == 'POST':
-        user = request.form['user']
+        username = request.form['user']
         password = request.form['pass']
-        if user == "saeed" and password == "pass":
+        if username == "saeed" and password == "pass":
 
-          # login_user(user)
-           return redirect('/')
+           login_user(user)
+           return redirect('/page3.html')
         if user != 'saeed' or password != 'pass' :
 
            flash('user or pass is wrong')
 
     return render_template('login.html')
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login.html')
+
+
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+
+
+
+
+
 
 
 if __name__ == "__main__":
