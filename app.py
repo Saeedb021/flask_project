37e756005bdd5d246341fb0028abf9273fc8f1e3
Saeed <saeedbarari021@gmail.com>
2023-09-08 01:36:58 +0330

    some small cleanup

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, flash ,Response,  render_template, request , redirect
 from flask_login import UserMixin ,current_user, LoginManager,login_user, logout_user,login_required
+import psycopg2
+import config
 
 login_manager = LoginManager()
 app = Flask(__name__)
 login_manager.init_app(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 login_manager.login_view = "login"
-import psycopg2
-import config
 
 conn = psycopg2.connect( host= config.host, database= config.database , user= config.user
         , password=config.password)
@@ -31,19 +31,16 @@ def insert_blog(url, title, body):
     sql = """INSERT INTO blog_tb(url, title, body) VALUES(%s, %s, %s);"""
 
     cur.execute(sql, (url, title, body))
-            # get the generated id back
-            # commit the changes to the database
+
+    # commit the changes to the database
     conn.commit()
-            # close communication with the database
+    # close communication with the database
     cur.close()
 
 
 def read_db(url):
     cur = conn.cursor()
 
-
-
-    #sql = """SELECT url, title , body FROM blog_tb WHERE url =%s;"""
     cur.execute("SELECT url, title , body FROM blog_tb WHERE url = %s ", (url,))
     all = cur.fetchall()
     if len(all) == 0:
@@ -52,18 +49,10 @@ def read_db(url):
         blog = all[0]
         title = blog[1]
         body = blog[2]
-    #print(title)
-    #print(body)
 
         cur.close()
 
         return(title, body)
-
-
-
-
-
-
 
 
 
@@ -80,11 +69,6 @@ def about():
 def contacts():
     return render_template('contacts.html')
 
-#@app.route('/<page>')
-#def pages (page):
-    #page_url = page + '.html'
-    #return render_template(page_url)
-
 
 @app.route('/post_blog', methods=['GET', 'POST'])
 @login_required
@@ -93,10 +77,9 @@ def blog ():
         url = request.form['Url']
         title = request.form['title']
         description = request.form['description']
-
-
         insert_blog(url, title, description)
-        #return show_blog(url)
+
+
         return redirect(f'/{url}')
     return render_template('post_blog.html')
 
@@ -109,7 +92,6 @@ def show_blog(page):
     else :
 
         title, body = read_db(page)
-
 
         text = body.split('\n')
         text= body.replace('\n', '<br>')
@@ -138,8 +120,6 @@ def login ():
 def logout():
     logout_user()
     return redirect('/login')
-
-
 
 
 @login_manager.user_loader
